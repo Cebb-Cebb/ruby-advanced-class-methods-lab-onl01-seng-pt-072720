fa4939db53eba82b248e8fdb967f0af8df77a26b
hersson ceballos <herssonceballos@gmail.com>
2020-08-31 13:48:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,8 +57,9 @@ class Song
     song
   end
   
-  def self.create_from_filename
-    
+  def self.create_from_filename(filename)
+    song_array =  filename.split("") 
+    binding.pry
   end 
   
   
